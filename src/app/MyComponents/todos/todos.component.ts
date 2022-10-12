8462b0de0da863c819bb2a2daa3279ab39f45ac5
joy <joybiswal123@gmail.com>
2022-10-12 18:18:34 +0530

    Changes2

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -7,35 +7,36 @@ import { Todo } from "../../Todo";
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
+   localItem: string;
    todos:Todo[];
   constructor() { 
-    this.todos = [
-      {
-        sno:1,
-        title: "This is title",
-        desc: "Description",
-        active: true
-      },
-      {
-        sno:2,
-        title: "This is title2",
-        desc: "Description",
-        active: true
-      },
-      {
-        sno:3,
-        title: "This is title3",
-        desc: "Description",
-        active: true
-      },
-    ]
+    this.localItem = localStorage.getItem("todos")!;//
+    if(this.localItem == null){
+    this.todos = [];
   }
+  else{
+    this.todos = JSON.parse(this.localItem);
+  }
+
+}
 
   ngOnInit(): void {
   }
 
   deleteTodo(todo:Todo){
     console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(this.todos));
   }
+  addTodo(todo:Todo){
+    console.log(todo);
+    this.todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+  toggleTodo(todo:Todo){
+    const index = this.todos.indexOf(todo);
+    this.todos[index].active = !this.todos[index].active;
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
 import { TodoItemComponent } from './MyComponenets/todo-item/todo-item.component';
+import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
